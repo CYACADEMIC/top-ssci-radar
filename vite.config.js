d4f8385076cf4bacf3ef211a818235a4f4caa7b9
CYACADEMIC <15553590893@163.com>
2026-05-25 11:21:19 +0800

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/top-ssci-radar/',
   plugins: [vue()],
   base: '/edutech-tracker/'
 })
